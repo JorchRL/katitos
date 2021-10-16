31af39fa567334fdfed456d8011aa35f3a180575
Jorch <jorch.rl@gmail.com>
2021-10-16 14:29:53 -0500

    Display cat photo on App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import "./App.css";
+import gato from "./imgs/katito.jpg";
 
 function App() {
     return (
         <div className='App'>
             <h2>Katitos :3</h2>
+            <img style={{ width: "100vw" }} src={gato} alt='Un katito' />
         </div>
     );
 }
