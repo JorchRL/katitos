f5e2a4361e3b33162a58f91ebef6e64b70c242be
Jorch <jorch.rl@gmail.com>
2021-12-05 13:48:39 -0600

    Add page sections layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,28 @@
 import "./app.scss";
 import gato from "./imgs/katito.jpg";
 import { useState } from "react";
+
 import Topbar from "./components/topbar/Topbar";
+import Menu from "./components/menu/Menu";
+import Intro from "./components/intro/Intro";
+import Portfolio from "./components/portfolio/Portfolio";
+import Works from "./components/works/Works";
+import Testimonials from "./components/testimonials/Testimonials";
+import Contact from "./components/contact/Contact";
 
 const App = () => {
   const [showMenu, setShowMenu] = useState(false);
   return (
     <div className='app'>
       <Topbar showMenu={showMenu} setShowMenu={setShowMenu} />
+      <Menu showMenu={showMenu} setShowMenu={setShowMenu} />
+      <div className='sections'>
+        <Intro />
+        <Portfolio />
+        <Works />
+        <Testimonials />
+        <Contact />
+      </div>
     </div>
   );
 };
