20566015fb71c918c0ec1f7b927330db4c7ad1d8
Jorch <jorch.rl@gmail.com>
2021-12-05 13:49:43 -0600

    Add template for contac, porfolio, testimonials and works components

---
+++ src/components/testimonials/Testimonials.jsx
@@ -0,0 +1,10 @@
+import "./testimonials.scss";
+const Testimonials = () => {
+  return (
+    <div className='testimonials' id='testimonials'>
+      testimonials
+    </div>
+  );
+};
+
+export default Testimonials;
