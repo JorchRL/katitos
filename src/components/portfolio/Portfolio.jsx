7235e446d7d5e900584795c34200c28248bc29ba
Jorch <jorch.rl@gmail.com>
2021-12-05 21:56:48 -0600

    Add porfolio component

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,9 +1,79 @@
+import { useEffect, useState } from "react";
 import "./portfolio.scss";
+import PortfolioList from "../portfolioList/PortfolioList";
+
+import {
+  featuredPortfolio,
+  webPortfolio,
+  mobilePortfolio,
+  designPortfolio,
+  contentPortfolio,
+} from "../../data";
+import { data } from "autoprefixer";
 
 const Portfolio = () => {
+  const [selected, setSelected] = useState("featured");
+  const [data, setData] = useState([]);
+  const list = [
+    {
+      id: "featured",
+      title: "Featured",
+    },
+    {
+      id: "web",
+      title: "Web Apps",
+    },
+    {
+      id: "mobile",
+      title: "Mobile Apps",
+    },
+    {
+      id: "design",
+      title: "Design",
+    },
+  ];
+
+  useEffect(() => {
+    switch (selected) {
+      case "featured":
+        setData(featuredPortfolio);
+        break;
+      case "web":
+        setData(webPortfolio);
+        break;
+      case "mobile":
+        setData(mobilePortfolio);
+        break;
+      case "design":
+        setData(designPortfolio);
+        break;
+      default:
+        setData(featuredPortfolio);
+    }
+  }, [selected]);
+
   return (
     <div className='portfolio' id='portfolio'>
-      portfolio
+      <h1>Portfolio</h1>
+      <ul>
+        {list.map((item) => (
+          <PortfolioList
+            key={item.id}
+            title={item.title}
+            active={selected === item.id}
+            setSelected={setSelected}
+            id={item.id}
+          />
+        ))}
+      </ul>
+      <div className='container'>
+        {data.map((d) => (
+          <div className='item'>
+            <img src={d.img} alt='' />
+            <h3>{d.title}</h3>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
